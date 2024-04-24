c7ae41337fedf8dee18b04178f57b87841977851
andreashgk <andreas@hgk.me>
2024-04-24 16:39:13 +0200

    frontend: Improve login UX

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,9 +1,3 @@
-"use client"
-
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { toast } from "sonner"
 import {
   Card,
   CardContent,
@@ -11,67 +5,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { useRouter } from "next/navigation"
-
-const formSchema = z.object({
-  username: z
-    .string()
-    .min(3, {
-      message: "Username must be at least 3 characters.",
-    })
-    .max(20, {
-      message: "Username must be at most 20 characters.",
-    }),
-  password: z
-    .string()
-    .min(8, {
-      message: "Password must be at least 8 characters",
-    })
-    .max(80, {
-      message: "Password must be at most 80 characters",
-    }),
-})
+import { LoginForm } from "@/components/login-form"
 
 export default function LoginPage() {
-  const router = useRouter()
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {},
-  })
-
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    let resp = await fetch("/api/sessions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(values),
-    })
-
-    if (resp.status == 401) {
-      toast("Username or password not found.")
-      return
-    }
-
-    if (!resp.ok) {
-      throw new Error("An error occurred while trying to authenticate")
-    }
-
-    toast("You have been logged in successfully.")
-    router.replace("/")
-  }
-
   return (
     <main className="flex flex-col justify-center flex-1">
       <div className="flex items-center justify-center flex-col gap-4">
@@ -81,44 +17,7 @@ export default function LoginPage() {
             <CardDescription>Please log in to continue.</CardDescription>
           </CardHeader>
           <CardContent>
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-8"
-              >
-                <FormField
-                  control={form.control}
-                  name="username"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Username</FormLabel>
-                      <FormControl>
-                        <Input placeholder="your username" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Password</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="password"
-                          placeholder="your password"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit">Submit</Button>
-              </form>
-            </Form>
+            <LoginForm />
           </CardContent>
         </Card>
       </div>
